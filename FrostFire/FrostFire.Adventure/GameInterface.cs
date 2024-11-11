61a6c2d51f2136947ae46a3c65bb1f4b1456a3e7
KhageshorGiri <girikhageshor2432@gmail.com>
2024-11-11 20:59:21 +0545

    Added Player Class

--- FrostFire/FrostFire.Adventure/GameInterface.cs
+++ FrostFire/FrostFire.Adventure/GameInterface.cs
@@ -1,25 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
+﻿namespace FrostFire.Adventure;
 
-namespace FrostFire.Adventure
+public partial class GameInterface : Form
 {
-    public partial class GameInterface : Form
+    public GameInterface()
     {
-        public GameInterface()
-        {
-            InitializeComponent();
-        }
+        InitializeComponent();
+    }
 
-        private void TestBtnClikcEvent(object sender, EventArgs e)
-        {
-            lblGold.Text = "Gold Here";
-        }
+    private void TestBtnClikcEvent(object sender, EventArgs e)
+    {
+        lblGold.Text = "Gold Here";
     }
 }
---
+++ FrostFire/FrostFire.Engine/Player.cs
@@ -0,0 +1,10 @@
+﻿namespace FrostFire.Engine;
+
+public class Player
+{
+    public int CurrentHitPoints { get; set; }
+    public int MaxHitPoints { get; set; }
+    public int Golds { get; set; }
+    public int ExperiencePoints { get; set; }
+    public int Level { get; set; }
+}
