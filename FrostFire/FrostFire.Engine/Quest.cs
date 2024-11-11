0a91e08ae04383b99e2085f2222d30df131fd086
KhageshorGiri <girikhageshor2432@gmail.com>
2024-11-11 21:21:16 +0545

    Added Other Classes

---
+++ FrostFire/FrostFire.Engine/Quest.cs
@@ -0,0 +1,10 @@
+﻿namespace FrostFire.Engine;
+
+public class Quest
+{
+    public int ID { get; set; }
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public int RewardExperiencePoints { get; set; }
+    public int RewardGold { get; set; }
+}
---
+++ FrostFire/FrostFire.Engine/Weapon.cs
@@ -0,0 +1,10 @@
+﻿namespace FrostFire.Engine;
+
+public class Weapon
+{
+    public int ID { get; set; }
+    public string Name { get; set; }
+    public string NamePlural { get; set; }
+    public int MinimumDamage { get; set; }
+    public int MaximumDamage { get; set; }
+}
