cf1ceb69ff14f2d36747b051014f66e274c87815
KhageshorGiri <girikhageshor2432@gmail.com>
2024-11-18 21:06:25 +0545

    Resolve Buid Error

--- FrostFire/FrostFire.Adventure/GameInterface.cs
+++ FrostFire/FrostFire.Adventure/GameInterface.cs
@@ -10,18 +10,6 @@ public partial class GameInterface : Form
     {
         InitializeComponent();
 
-        _player = new Player();
-
-        _player.CurrentHitPoints = 10;
-        _player.MaxHitPoints = 10;
-        _player.Golds = 10;
-        _player.ExperiencePoints = 0;
-        _player.Level = 1;
-
-        lblHitPoints.Text = _player.CurrentHitPoints.ToString();
-        lblGold.Text = _player.Golds.ToString();
-        lblExperience.Text = _player.ExperiencePoints.ToString();
-        lblLevel.Text = _player.Level.ToString();
     }
 
  
