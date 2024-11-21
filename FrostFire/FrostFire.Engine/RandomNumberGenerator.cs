59ab7ff09c3572313f16f26d765cfd664bdfad93
KhageshorGiri <girikhageshor2432@gmail.com>
2024-11-21 20:51:38 +0545

    Add Random Number Generator

---
+++ FrostFire/FrostFire.Engine/RandomNumberGenerator.cs
@@ -0,0 +1,11 @@
+﻿namespace FrostFire.Engine;
+
+public static class RandomNumberGenerator
+{
+    private static Random _random = new Random();
+
+    private static int GetRandomNumberBetween(int min, int max)
+    {
+        return _random.Next(min, max);
+    }
+}
