6a9c726026caa291ca5fe13e9ac45aa25db28768
KhageshorGiri <girikhageshor2432@gmail.com>
2024-11-21 20:47:00 +0545

    Added UI Event Handler

--- FrostFire/FrostFire.Adventure/GameInterface.cs
+++ FrostFire/FrostFire.Adventure/GameInterface.cs
@@ -5,12 +5,40 @@ namespace FrostFire.Adventure;
 public partial class GameInterface : Form
 {
     private Player _player;
-        
+
     public GameInterface()
     {
         InitializeComponent();
 
     }
 
- 
+    private void btnUseWeapon_Click(object sender, EventArgs e)
+    {
+
+    }
+
+    private void btnUsePotion_Click(object sender, EventArgs e)
+    {
+
+    }
+
+    private void btnEast_Click(object sender, EventArgs e)
+    {
+
+    }
+
+    private void btnWest_Click(object sender, EventArgs e)
+    {
+
+    }
+
+    private void btnNorth_Click(object sender, EventArgs e)
+    {
+
+    }
+
+    private void btnSouth_Click(object sender, EventArgs e)
+    {
+
+    }
 }
